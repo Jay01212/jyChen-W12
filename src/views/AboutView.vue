d5c727eeb036ea29e7a434988f896b8be083d99d
Jay01212 <jche0295@student.monash.edu>
2024-10-18 17:09:36 +1100

    Trigger deployment

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,7 @@
     <div class="about">
       <h1 class="text-center">About Our Library</h1>
       <p class="text-center">Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+      <p class="text-center">small change</p>
     </div>
   </div>
 </template>
